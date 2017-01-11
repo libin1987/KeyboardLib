3a0245108abc3c055e3a73b7824d2f8eb4efa1f5
libin1987 <lib1987@qq.com>
2017-01-11 15:18:43 +0800

    更新

---
+++ app/src/main/java/cn/qing/soft/customkeyboard/MainActivity.java
@@ -0,0 +1,38 @@
+package cn.qing.soft.customkeyboard;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+
+import cn.qing.soft.keyboardlib.CustomKeyboardHelper;
+
+public class MainActivity extends AppCompatActivity {
+
+    CustomKeyboardHelper helper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        helper = new CustomKeyboardHelper(this, R.xml.keyboardnumber);
+
+        EditText editText1 = (EditText) findViewById(R.id.editText1);
+        EditText editText2 = (EditText) findViewById(R.id.editText2);
+        EditText editText3 = (EditText) findViewById(R.id.editText3);
+
+        // 固定数字键盘
+        helper.registerEditText(editText1);
+        // 随机数字键盘
+        helper.registerEditText(editText3, true);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (helper.isCustomKeyboardVisible()) {
+            helper.hideCustomKeyboard();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
